d5da02942cfb9bcc1a78ef125039fcddb482b379
Silva, Rodolpho Mendes Caires <Rodolpho.Silva@dieboldnixdorf.com>
2019-10-06 20:08:38 -0300

    Proper .gitignore

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,19 +2,28 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
+const socketio = require('socket.io');
+const http = require('http');
 
 const routes = require('./routes');
 
 const app = express();
 
+const server = http.Server(app);
+const io = socketio(server);
+
+io.on('connection', socket => {
+    console.log('Usuário conectado', socket.id);
+});
+
 mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-4jue2.mongodb.net/semana09?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-})
+});
 
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
